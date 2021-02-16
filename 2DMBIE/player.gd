05ce57ab8b9f7cb2c692a3edef0e1bbd96b04d1b
Floris <florisgra@gmail.com>
2021-02-16 11:30:58 +0100

    Animation + MovementSpeed Anton

--- 2DMBIE/player.gd
+++ 2DMBIE/player.gd
@@ -2,23 +2,50 @@ extends KinematicBody2D
 
 var velocity = Vector2(0,0)
 
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const ACCELERATION = 20
+const MAX_SPEED = 110 #500
+const JUMP_HEIGHT = -600
+
+var motion = Vector2()
+
 func _ready():
 	$AnimationTree.active = true
 
-func _process(delta):
+func _physics_process(delta):
+	motion.y += GRAVITY
+	var friction = false
+	
 	if Input.is_action_pressed("left"):
-		velocity.x = -100
+		motion.x -= ACCELERATION
+		motion.x = max(motion.x, -MAX_SPEED)
 		$AnimationTree.set("parameters/walk-idle/blend_amount", 0)
 		$torso.scale = Vector2(-1,1)
+		
 	elif Input.is_action_pressed("right"):
+		motion.x += ACCELERATION
+		motion.x = min(motion.x, MAX_SPEED)
 		$AnimationTree.set("parameters/walk-idle/blend_amount", 0)
-		velocity.x = 100
 		$torso.scale = Vector2(1,1)
-	elif Input.is_action_pressed("down"):
-		pass
-	walk_idle_transition()
-	move_and_slide(velocity)
-	velocity.x = lerp(velocity.x,0,0.2)
+		
+	else:
+		friction = true
+		#print(motion.x)
+		motion.x = lerp(motion.x, 0, 0.3)	
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("up"):
+			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.3)
+			
+	else:
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
+		
+	motion = move_and_slide(motion, UP)
+	pass
 	
 func walk_idle_transition():
 	var speed = velocity.x
