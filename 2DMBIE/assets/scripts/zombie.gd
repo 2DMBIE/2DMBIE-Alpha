680c5505cddc0caef65cc029e6c3f4d65430e404
ivardeviking <ivar.stek@student.hu.nl>
2021-03-16 09:26:14 +0100

    logoblock script toepassen op zombies script, en het instellen van collisions

--- 2DMBIE/assets/scripts/zombie.gd
+++ 2DMBIE/assets/scripts/zombie.gd
@@ -52,3 +52,25 @@ func step():
 
 func togglestep():
 	zombiestep = !zombiestep
+
+signal health_updated(health)
+
+export (float) var maxHealth = 500
+export (float) var enemyDamage = 300
+
+onready var health = maxHealth setget _set_health
+
+func Hurt(damage):
+	_set_health(health - damage)
+	print(health)
+
+func kill():
+	queue_free()
+
+func _set_health(value):
+	var prevHealth = health
+	health = clamp(value, 0, maxHealth)
+	if health != prevHealth:
+		emit_signal("health_updated", health)
+		if health == 0:
+			kill()
