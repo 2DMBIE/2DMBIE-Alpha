31f3310cdb3cdcb0fa64f30b4c128a3c4b1dac2b
Joas Boevink <joas.boevink@kpnmail.nl>
2021-04-22 11:37:34 +0200

    Repair Zombie Script
    All functions in old script transferred to new script.
    Sounds and animations still bugged

--- 2DMBIE/assets/scenes/zombie.gd
+++ 2DMBIE/assets/scenes/zombie.gd
@@ -8,11 +8,31 @@ var speed = 200
 var jumpForce = 400
 var gravity = 600
 var padding = 2
-var finishPadding = 4
+var finishPadding = 4 # 6 or 8 for better padding when state machine
 
 var movement
+var zombiestep = false
+signal play_sound(library)
+export (int) var growl_time_min = 5 
+export (int) var growl_time_max = 12
+var growl_timer = Timer.new()
+var _time_diff = growl_time_max - growl_time_min 
+var _wait_time = randi()%_time_diff + growl_time_min
+
+onready var health = maxHealth setget _set_health
+signal health_updated(health)
+export (float) var maxHealth = 500
+export (float) var enemyDamage = 300
 
 func _ready():
+	$AnimationTree.active = true
+	$AnimationTree.set("parameters/walk/current", randi()%10)
+	growl_timer.wait_time = _wait_time
+	growl_timer.one_shot = false
+	growl_timer.connect("timeout", self, "growl")
+	growl_timer.autostart = true
+	add_child(growl_timer)
+	
 	pathFinder = find_parent("Main").find_node("Pathfinder")
 	movement = Vector2(0, 0)
 	var timer = Timer.new()
@@ -36,15 +56,15 @@ func nextPoint():
 func jump():
 	if (self.is_on_floor()):
 		movement[1] = -jumpForce
-	
+
 func _process(delta):
 #	if Input.is_action_just_pressed("aim"):
 #		repeat_me()
 
 	if currentTarget:
-		if (currentTarget[0] - padding > position[0]): # and position.distance_to(currentTarget) > padding:
+		if (currentTarget[0] - padding > position[0]) and position.distance_to(currentTarget) > padding:
 			movement[0] = speed
-		elif (currentTarget[0] + padding < position[0]): # and position.distance_to(currentTarget) > padding:
+		elif (currentTarget[0] + padding < position[0]) and position.distance_to(currentTarget) > padding:
 			movement[0] = -speed
 		else:
 			movement[0] = 0
@@ -58,9 +78,18 @@ func _process(delta):
 		movement[1] += gravity * delta
 	elif movement[1] > 0:
 		movement[1] = 0
+		
+	if !is_on_floor():
+		$AnimationTree.set("parameters/in_air/current", 0)
+	else:
+		$AnimationTree.set("parameters/in_air/current", 1)
 	
 	var _moveSlide = self.move_and_slide(movement, Vector2(0, -1))
-	
+	if self.movement.x < 0:
+		direction("left")
+	else:
+		direction("right")
+
 func repeat_me():
 	if is_on_floor():
 		var space_state = get_world_2d().direct_space_state
@@ -71,3 +100,60 @@ func repeat_me():
 			var goTo = result["position"]
 			currentPath = pathFinder.findPath(self.position, goTo)
 			nextPoint()
+
+func direction(x):
+	var body = get_node("body")
+	if (x == "left") && !(body.scale == Vector2(-1,1)):
+		body.scale = Vector2(-1,1)
+	elif (x == "right") && !(body.scale == Vector2(1,1)):
+		body.scale = Vector2(1,1)
+	else: pass
+
+func growl():
+	emit_signal("play_sound", "growl")
+
+#func togglestep():
+#	zombiestep = !zombiestep
+
+func show_damage_animation(_health_percentage):
+	var _index
+	var _array = [Color("ffcbcb"), Color("ff9f9f"), Color("ff7e7e"), Color("ff5858"), Color("ffe7e7")] #Color("ffcbcb")
+	if _health_percentage < 100 and _health_percentage >= 80:
+		_index = 0
+	elif _health_percentage < 80 and _health_percentage >= 60:
+		_index = 1
+	elif _health_percentage < 60 and _health_percentage >= 40:
+		_index = 2
+	elif _health_percentage < 40 and _health_percentage >= 20:
+		_index = 3
+	elif _health_percentage < 20:
+		_index = 4
+	
+	var _timer = Timer.new()
+	_timer.one_shot = true
+	_timer.wait_time = 0.15
+	_timer.connect("timeout", self, "_reset_module")
+	add_child(_timer)
+	modulate = _array[_index]
+	_timer.start()
+
+func _reset_module():
+	modulate = Color("ffffff")
+
+func Hurt(damage):
+	_set_health(health - damage)
+	var percentage = health/maxHealth*100
+	show_damage_animation(percentage)
+	emit_signal("play_sound", "hurt")
+
+func kill():
+	Global.Score += Global.ScoreIncrement
+	queue_free()
+
+func _set_health(value):
+	var prevHealth = health
+	health = clamp(value, 0, maxHealth)
+	if health != prevHealth:
+		emit_signal("health_updated", health)
+		if health == 0:
+			kill()
