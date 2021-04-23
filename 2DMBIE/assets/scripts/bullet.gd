a97c84101dec964091043812668ee0cb3e9084ce
ivardeviking <ivar.stek@student.hu.nl>
2021-04-23 10:48:42 +0200

    bullet penetration

--- 2DMBIE/assets/scripts/bullet.gd
+++ 2DMBIE/assets/scripts/bullet.gd
@@ -3,6 +3,8 @@ extends Area2D
 export (float) var damage = 300
 var speed = 750
 var direction := Vector2.ZERO
+var bulletpenatration = 2
+var enemyPenetration = 0
 
 func _physics_process(delta):
 	if direction != Vector2.ZERO:
@@ -10,9 +12,9 @@ func _physics_process(delta):
 		global_position += velocity
 
 func _on_bullet_body_entered(body):	
+	var enemyPenetration = 0
 	if body.is_in_group("enemies"):
 		body.Hurt(damage)
-		queue_free()
 	else:
 		queue_free()
 
@@ -21,3 +23,10 @@ func set_direction(directionx: Vector2):
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+func _on_bullet_body_exited(body):
+	if body.is_in_group("enemies"):
+#		visible = false
+		enemyPenetration += 1
+		if enemyPenetration >= bulletpenatration:
+			queue_free()
