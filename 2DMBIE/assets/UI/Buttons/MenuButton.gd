277c3703d03094f6f575a6dae679815119261f2a
TijmenSimons <70627990+TijmenSimons@users.noreply.github.com>
2021-05-12 16:56:54 +0200

    All Buttons now work
    All Buttons now work, and no errors

--- 2DMBIE/assets/UI/Buttons/MenuButton.gd
+++ 2DMBIE/assets/UI/Buttons/MenuButton.gd
@@ -1,3 +1,11 @@
 extends Button
 
 export(String) var scene_to_load
+
+
+func _on_Area2D_mouse_entered():
+	print("mouse entered")
+
+
+func _on_Area2D_mouse_exited():
+	print("mouse exited")
--- 2DMBIE/assets/scripts/title_screen.gd
+++ 2DMBIE/assets/scripts/title_screen.gd
@@ -11,3 +11,7 @@ func _on_Button_pressed(scene_to_load):
 	var _x = get_tree().change_scene(scene_to_load)
 
 
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
