f2ba2ca2d20525e1ef899c3faa9b53786d7f6020
ivar <ivar.stek@student.hu.nl>
2021-05-18 14:09:06 +0200

    fixed weapon slots

--- 2DMBIE/assets/scripts/Weapon1.gd
+++ 2DMBIE/assets/scripts/Weapon1.gd
@@ -1,16 +1,12 @@
 extends Control
 
-var spriteMP5 = preload("res://assets/sprites/guns/mp5.png")
-var spriteSPAS12 = preload("res://assets/sprites/guns/spas12.png")
-var spriteM4A1 = preload("res://assets/sprites/guns/m4a1.png")
-var spriteAK12 = preload("res://assets/sprites/guns/ak12.png")
-var spriteBARRETT50 = preload("res://assets/sprites/guns/barrett50.png")
-var spriteArray = [spriteMP5, spriteSPAS12, spriteM4A1, spriteAK12, spriteBARRETT50]
-
 onready var gunscript = get_node("../../../Player/body/chest/torso/gun")
 
-func _physics_process(_delta):
-	$Sprite.texture = gunscript.guns[gunscript.weapon_slots[0]].texture
-
-
+func _process(_delta):
+	$Weapon1/Sprite.texture = gunscript.guns[gunscript.weapon_slots[0]].texture
+	$Weapon2/Sprite.texture = gunscript.guns[gunscript.weapon_slots[1]].texture
 	
+	if gunscript.weapon_slots[1] == -1:
+		$Weapon2.visible = false
+	else:
+		$Weapon2.visible = true
