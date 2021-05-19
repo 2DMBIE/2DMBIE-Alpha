2b8571a6328bbbc8ed6abcbb2e8c55c6aed0d601
Anton van Someren <acrbvansomeren@gmail.com>
2021-05-19 13:15:56 +0200

    Oude noob not inherited manier

--- 2DMBIE/assets/scripts/perks/MovementPerk.gd
+++ 2DMBIE/assets/scripts/perks/MovementPerk.gd
@@ -1,16 +1,16 @@
-extends Perk
-
-class_name MovementPerk
-
-signal on_movementperk_event(received)
-
-func _init():
-	name = "MovementPerk"
-	price = "4000"
-	texture = load("res://assets/sprites/perks/movement_perk.png")
-
-func activate():
-	emit_signal("on_movementperk_event", true)
-
-func deactivate():
-	emit_signal("on_movementperk_event", false)
+#extends Perk
+#
+#class_name MovementPerk
+#
+#signal on_movementperk_event(received)
+#
+#func _init():
+#	name = "MovementPerk"
+#	price = "4000"
+#	texture = load("res://assets/sprites/perks/movement_perk.png")
+#
+#func activate():
+#	emit_signal("on_movementperk_event", true)
+#
+#func deactivate():
+#	emit_signal("on_movementperk_event", false)
--- 2DMBIE/assets/scripts/perks/purchasable_perks.gd
+++ 2DMBIE/assets/scripts/perks/purchasable_perks.gd
@@ -1,13 +1,49 @@
 extends Node
 
-onready var healthperk = get_node("HealthPerk")
-onready var movementperk = get_node("MovementPerk")
+var spriteHealthPerk = preload("res://assets/sprites/perks/health_perk.png")
+var spriteMovementPerk = preload("res://assets/sprites/perks/movement_perk.png")
+var spriteArray = [spriteHealthPerk, spriteMovementPerk]
+var colorArray = [Color.limegreen, Color.turquoise]
+var nameArray = ["HealthPerk", "MovementPerk"]
+var priceArray = ["1500", "2500"]
 
-#var perks = [HealthPerk.new(), MovementPerk.new()]
+var canBuy = false
+var enoughMoney = false
+
 export(int, "Health", "Movement speed") var Selected_Perk = 0
 
+func _on_buyarea_body_entered(body):
+	if body.is_in_group("player"):
+		canBuy = true
+#		print("player entered the area")
+#		print(canBuy)
+
+# checks if the player has enough money/score
+	for joas in range(spriteArray.size()):
+		if Selected_Perk == joas:
+			if Global.Score >= int(priceArray[joas]):
+				enoughMoney = true
+#				print(enoughMoney)
+			else:
+				enoughMoney = false
+#				print(enoughMoney)
+
+#checks if the player is out of the buy area
+func _on_buyarea_body_exited(body):
+	if body.is_in_group("player"):
+		canBuy = false
+#		print("player exited the area")
+#		print(canBuy)
+
+func _ready():
+	$Sprite.set_texture(spriteArray[Selected_Perk])
+	$Light2D.color = colorArray[Selected_Perk]
+	$PerkLabelName.text = nameArray[Selected_Perk]
+	$PerkLabelPrice.text = priceArray[Selected_Perk]
+
 # Called when the node enters the scene tree for the first time.
-func _ready():	
-	#var x = perks[Selected_Perk].activate()
-	healthperk.activate()
-	movementperk.activate()
+#func _ready():	
+#	#var x = perks[Selected_Perk].activate()
+##	healthperk.activate()
+##	movementperk.activate()
+#	pass
