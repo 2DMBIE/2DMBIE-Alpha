1d3be6bf4a0db2aff347fcb3276c12f20f85b237
Floris Gravendeel <florisgra@gmail.com>
2021-05-28 12:30:59 +0200

    Basic connection!

---
+++ 2DMBIE/assets/scripts/server.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var network = NetworkedMultiplayerENet.new()
+var port = 25565
+var max_players = 4
+
+var players = {}
+var ready_players = 0
+
+func _ready():
+	start_server()
+	
+func start_server():
+	network.create_server(port, max_players)
+	get_tree().set_network_peer(network)
+	network.connect("peer_connected", self, "_player_connected")
+	network.connect("peer_disconnected", self, "_player_disconnected")
+	print("Server Started")
+	
+func _player_connected(player_id):
+	print("Player: " + str(player_id) + " Connected")
+	
+func _player_disconnected(player_id):
+	print("Player: " + str(player_id) + " Disconnected")
+	players.erase(player_id)
