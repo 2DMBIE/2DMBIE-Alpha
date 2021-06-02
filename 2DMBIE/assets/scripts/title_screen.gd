06d7e4aa9adf8c88657f1474f48b921abe69fd6c
TijmenSimons <70627990+TijmenSimons@users.noreply.github.com>
2021-06-02 12:25:23 +0200

    Added Multiplayer Menu Button
    Added the button
    Made the main menu buttons change
    Fixed the hoverOn-stuck bug.
    Might want to relocate where you get sent by the singel and multiplayer buttons

--- 2DMBIE/assets/scripts/title_screen.gd
+++ 2DMBIE/assets/scripts/title_screen.gd
@@ -2,11 +2,30 @@ extends Node2D
 
 
 func _ready():
-	for button in $Buttons.get_children():
-		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+	pass
 
-func _on_Button_pressed(scene_to_load):
-	var _x = get_tree().change_scene(scene_to_load)
 
 func _on_ExitButton_pressed():
 	get_tree().quit()
+
+
+func _on_PlayButton_button_down():
+	get_node("Buttons/Main").visible = false
+	get_node("Buttons/Play").visible = true
+
+
+func _on_BackButton_button_down():
+	get_node("Buttons/Main").visible = true
+	get_node("Buttons/Play").visible = false
+
+
+func _on_SinglePlayer_button_down():
+	var _x = get_tree().change_scene("res://assets/scenes/level3.tscn")
+
+
+func _on_Multiplayer_button_down():
+	var _x = get_tree().change_scene("res://assets/scenes/lobby.tscn")
+
+
+func _on_OptionsButton_button_down():
+	var _x = get_tree().change_scene("res://assets/UI/OptionsMenu.tscn")
