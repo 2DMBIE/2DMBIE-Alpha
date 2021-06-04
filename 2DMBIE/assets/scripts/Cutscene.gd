af104df9b66f69bda4c7e903a592ee3fe7ee182b
TijmenSimons <70627990+TijmenSimons@users.noreply.github.com>
2021-06-04 11:40:07 +0200

    Finished Cutscene

--- 2DMBIE/assets/scripts/Cutscene.gd
+++ 2DMBIE/assets/scripts/Cutscene.gd
@@ -2,9 +2,19 @@ extends Node2D
 
 
 onready var anim = $AnimationPlayer
-
+var numder = 0
 
 func _ready():
 	anim.play("Cutscene")
+	$Timer.start(26)
+	
 
+func _on_Timer_timeout():
+	get_node("Label").visible = !get_node("Label").visible
+	$Timer.start(1)
 
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.scancode != KEY_ENTER:
+			numder += 1
+			print('skip cutscene! '+str(numder))
