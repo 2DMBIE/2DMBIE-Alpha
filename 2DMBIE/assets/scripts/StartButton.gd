f7d674ec53624be194fcb7981bf39488f515c555
Floris Gravendeel <florisgra@gmail.com>
2021-06-05 16:20:33 +0200

    removed unnecessary  comments

--- 2DMBIE/assets/scripts/StartButton.gd
+++ 2DMBIE/assets/scripts/StartButton.gd
@@ -6,6 +6,6 @@ var user_amount = 0
 func _ready():
 	total_user_amount = gamestate.MAX_PEERS + 1
 
-func _process(delta):
+func _process(_delta):
 	user_amount = gamestate.players.size() + 1
 	$Control/Panel/Label.text = str(user_amount) + " / " + str(total_user_amount)
